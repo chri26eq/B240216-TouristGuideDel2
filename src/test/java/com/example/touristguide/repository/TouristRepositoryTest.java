a27bbffd192c921f8f92b54f89bd3ed69cf3c0b9
chri26eq <chri26eq@stud.kea.dk>
2024-02-28 10:52:40 +0100

    a

---
+++ src/test/java/com/example/touristguide/repository/TouristRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.example.touristguide.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TouristRepositoryTest {
+
+    @Test
+    void addAttraction() {
+
+    }
+
+    @Test
+    void updateAttraction() {
+
+    }
+
+    @Test
+    void deleteAttraction() {
+
+    }
+
+    @Test
+    void findByName() {
+
+    }
+
+    @Test
+    void getAllTags() {
+
+    }
+
+    @Test
+    void getAllTowns() {
+
+    }
+}
